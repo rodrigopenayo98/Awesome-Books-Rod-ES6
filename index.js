147c726086b7195970de902bb5710ec116d825db
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-05-29 14:19:10 -0300

    add index.html and index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let add = document.querySelector("#add");
+let bookList = document.querySelector("#book-list");
+
+
+let list = JSON.parse(localStorage.getItem("bookList")) || [
+  {
+    titleBook: "Martin Fierro",
+    authorBook: "José Hernandez",
+  },
+  {
+    titleBook: "Harry Potter and the Chamber of Secrets",
+    authorBook: "J. K. Rowling",
+  },
+];
+
+let htmlList = "";
+
+function addBook() {
+  const newBook = {
+    titleBook: title.value,
+    authorBook: author.value,
+  };
+
+  list.push(newBook);
+
+  renderBooks();
+  saveToLocalStorage();
+}
+
+function removeBook(event) {
+  const button = event.target;
+  const bookContainer = button.parentNode;
+  const bookTitle = bookContainer.querySelector(".title").textContent;
+
+  const updatedList = list.filter((book) => book.titleBook !== bookTitle);
+
+  list = updatedList;
+
+  renderBooks();
+  saveToLocalStorage();
+}
+
+function renderBooks() {
+  htmlList = "";
+
+  list.forEach((book) => {
+    htmlList += `
+    <div>
+      <p>
+        <span class="title">${book.titleBook}</span>
+        <br>
+        ${book.authorBook}
+        <br>
+        <button class="remove">Remove</button>
+      </p>
+      <hr>
+    </div>`;
+  });
+
+  bookList.innerHTML = htmlList;
+
+  const removeButtons = document.querySelectorAll(".remove");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", removeBook);
+  });
+}
+
+
+renderBooks();
+
+add.addEventListener("click", addBook);
+
